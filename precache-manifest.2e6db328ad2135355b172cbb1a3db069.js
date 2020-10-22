e52d2f22cb4ed7c53a072de88c2cc35fb874cb6b
doppelgunner <doppelgunner@gmail.com>
2020-10-22 09:07:21 +0800

    Updates

---
+++ precache-manifest.2e6db328ad2135355b172cbb1a3db069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546d83c035c4eb41236a59f09cb5e98b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4bdce36f9b86f6238b06",
+    "url": "/portfolio/static/css/2.08834da3.chunk.css"
+  },
+  {
+    "revision": "9d69ea5afd85088b617d",
+    "url": "/portfolio/static/css/main.c147135c.chunk.css"
+  },
+  {
+    "revision": "4bdce36f9b86f6238b06",
+    "url": "/portfolio/static/js/2.41b3cc8b.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/portfolio/static/js/2.41b3cc8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d69ea5afd85088b617d",
+    "url": "/portfolio/static/js/main.87167bf4.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
