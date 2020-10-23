041a58fd27070dd1d98f99c441a4565aaa10b741
doppelgunner <doppelgunner@gmail.com>
2020-10-23 14:18:34 +0800

    Updates

---
+++ precache-manifest.a26f1d8db21afbebf582db19c9ca3d01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985037ee28758e02e2d70fa45411773f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f1dd0042509cc7e6f35e",
+    "url": "/portfolio/static/css/2.08834da3.chunk.css"
+  },
+  {
+    "revision": "9fc3b12e918014a8dfce",
+    "url": "/portfolio/static/css/main.20bc600b.chunk.css"
+  },
+  {
+    "revision": "f1dd0042509cc7e6f35e",
+    "url": "/portfolio/static/js/2.7cf8f542.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/portfolio/static/js/2.7cf8f542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc3b12e918014a8dfce",
+    "url": "/portfolio/static/js/main.acad79ff.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b8d49163fbe67eac8419ca55cc6dde85.js"
+  "/portfolio/precache-manifest.a26f1d8db21afbebf582db19c9ca3d01.js"
 );
 
 self.addEventListener('message', (event) => {
